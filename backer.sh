c17fdd081a99ea6a1e0e52799ab67750d47ef16c
Pau Sauleda <pau.sauleda@gmail.com>
2012-05-31 21:01:32 +0200

    leer los directorios destino desde un fichero

--- backer.sh
+++ backer.sh
@@ -4,11 +4,26 @@
 #
 # el enlace $DIRBACKUPS/ultimo apunta a la última copia realizada, sobre la cual se crea la incremental
 # si no existe este enlace, entiende que es la primera copia y crea la copia base
+#
+# los backups se realizan en $DIRBACKUPS
+# los directorios a respaldar se leen del archivo backer.conf, en el mismo directorio del script
+#
+
 
 TIMESTAMP=`date +%Y-%m-%d_%H-%M-%S`
 
 DIRBACKUPS=/backups
-DIRSACOPIAR="/etc /var/www"
+RUTAFICH=`/usr/bin/dirname $0`
+FICHDIRS=$RUTAFICH/backer.conf
+
+while read LINEA
+do
+	if [[ !($LINEA =~ ^#) ]] # si la linea no empieza por almohadilla
+	then
+	   DIRSACOPIAR="$DIRSACOPIAR $LINEA" # agrega la linea a la lista de directorios
+	fi
+done < $FICHDIRS
+
 
 if [ ! -h $DIRBACKUPS/ultimo ]
 then
